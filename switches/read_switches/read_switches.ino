e331b0bfe1f58f81adf8daafc0e0b35c1d3f0736
Anca Mosoiu <anca@anca.tv>
2015-07-07 08:41:36 -0700

    Multiplexer can read 4 switches

--- switches/read_switches/read_switches.ino
+++ switches/read_switches/read_switches.ino
@@ -4,8 +4,8 @@
  */
  
 int numPins = 2;
-int out[] = {4,5};
-int in[] = {2,3};
+int out[] = {4,5};  //For output
+int in[] = {2,3};   //For input
 
 
 void setup(){
@@ -14,7 +14,7 @@ void setup(){
   Serial.println("Initializing!");
   Serial.println("Pins: ");
 
-  // make the pushbutton's pin an input:
+  // Set the pinmode (do we need to do that?)
   for (int i=0; i < numPins; i++){
     pinMode(in[i], INPUT);
     pinMode(out[i], OUTPUT);
@@ -29,15 +29,12 @@ void setup(){
 
  
 void loop(){
-    digitalWrite(4,LOW);
-    
-    digitalWrite(5,LOW);
-   checkSwitch();
+   checkSwitches();
    delay(100);
   
 }
 
-
+/* Check to see if the switches are working
 int checkSwitch(){  
   
 if (digitalRead(0)==LOW){
@@ -53,73 +50,42 @@ if (digitalRead(3)==LOW){
    Serial.println("low pin3");
   }
 }
- 
- //  int val, row, column = 0;
-//  
-//  for(int i =0; i<numPins; i++){
-//      digitalWrite(out[i], LOW);
-//  }
-//  
-//  
-//  for(int i=0; i<numPins; i++){
-//    row = i;
-//    // get the row
-//    digitalWrite(out[i], HIGH);
-//    for(int j=0; j<numPins; j++){
-//     
-//      val = digitalRead(in[j]);
-//      if (val == HIGH){
-//        column = j;
-//        break;
-//      }
-//    }    
-//    if (column > 0){
-//       break;
-//    } 
-//    digitalWrite(out[i], LOW);
-//  }
-//  if (column > 0){
-//    Serial.print("Pushed: {");
-//    Serial.print(row);
-//    Serial.print(" ");
-//    Serial.print(column);
-//    Serial.println("}");
-//  }
-//  return 1;
-//}
+*/
 
-//int checkSwitches(){  
-// 
-//  int val, row, column = 0;
-//  
-//  for(int i =0; i<numPins; i++){
-//      digitalWrite(out[i], LOW);
-//  }
-//  
-//  
-//  for(int i=0; i<numPins; i++){
-//    row = i;
-//    // get the row
-//    digitalWrite(out[i], HIGH);
-//    for(int j=0; j<numPins; j++){
-//     
-//      val = digitalRead(in[j]);
-//      if (val == HIGH){
-//        column = j;
-//        break;
-//      }
-//    }    
-//    if (column > 0){
-//       break;
-//    } 
-//    digitalWrite(out[i], LOW);
-//  }
-//  if (column > 0){
-//    Serial.print("Pushed: {");
-//    Serial.print(row);
-//    Serial.print(" ");
-//    Serial.print(column);
-//    Serial.println("}");
-//  }
-//  return 1;
-//}
+int checkSwitches(){  
+ 
+  int val, row, column = 0;
+  
+  for(int i =0; i<numPins; i++){
+      digitalWrite(out[i], LOW);
+  }
+  
+  
+  for(int i=0; i<numPins; i++){
+    row = i;
+    // get the row
+    digitalWrite(out[i], HIGH);
+    column= -1;
+    for(int j=0; j<numPins; j++){
+     
+      val = digitalRead(in[j]);
+      if (val == LOW){
+        column = j;
+        break;
+      }
+    }    
+    if (column >= 0){
+      digitalWrite(out[i], LOW);
+      break;
+    } 
+    digitalWrite(out[i], LOW);
+  }
+  if (column >= 0){
+    Serial.print("Pushed: {");
+    Serial.print(row);
+    Serial.print(" ");
+    Serial.print(column);
+    Serial.println("}");
+  }
+  return 1;
+}
